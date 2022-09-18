d5160c8013947d063edd047faa75f637d7ba2733
Ubuntu <ubuntu@ip-172-31-6-129.us-east-2.compute.internal>
2022-09-18 20:12:11 +0000

    add starter file

---
+++ seta.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+sudo docker network create chiroposture_network
+sudo docker network create meylorCI
+
+# useiam services
+git clone git@gitlab.com:devs176/useiam.git
+cd ~/useiam
+sudo ./scripts/start-prod.sh
+cd ~
+
+git clone git@gitlab.com:devs176/useiam-server.git
+cd ~/useiam-server
+sudo ./scripts/start.sh
+cd ~
+
+# django zillow services
+git clone git@gitlab.com:a4496/python-base.git
+cd ~/python-base
+sudo ./scripts/local_db.sh
+cd ~
+
+git clone git@gitlab.com:a4496/django-zillow.git
+cd ~/django-zillow
+sudo ./scripts/start.sh
+sudo ./scripts/start-pg-bouncer.sh
+sudo ./scripts/start-db.sh
+# start-es.sh
+cd ~
+
+
+git clone git@gitlab.com:devs176/teacher-ui.git
+cd ~/teacher-ui/
+sudo ./scripts/start.sh
+cd ~
+
+git clone git@gitlab.com:a4496/meylorci.git
+cd ~/meylorci
+sudo ./scripts/start.sh
+sudo ./scripts/local_db.sh
+cd ~
+
+
