32c0769f4270e0c326c0127f77547167ee9aee9b
AR <a@dreampotential.org>
2022-10-03 18:41:37 +0000

    add site-enabled configs

---
+++ getcert.sh
@@ -0,0 +1,2 @@
+sudo certbot --nginx -d alt-r.world -d www.alt-r.world -d api.alt-r.world -d python-base-api.alt-r.world -d teacher.alt-r.world -d teacher-v2.alt-r.world -d python-base-api.alt-r.world -d python-base-api-wss.alt-r.world
+
--- install.sh
+++ install.sh
@@ -4,7 +4,13 @@ git config --global core.editor "vim"
 
 sudo apt-get update
 
-sudo apt-get install git docker.io python3-pip python3-virtualenv
+sudo apt-get install git docker.io python3-pip python3-virtualenv certbot python3-certbot-apache net-tools
+sudo apt install certbot python3-certbot-nginx npm
+
+# python-base
+sudo apt install libffi-dev cmake libjpeg-dev zlib1g-dev
+
+
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
