1ce4f71ccafa64af96828cc52608c62b257d9442
Ben viP <bengg2040@gmail.com>
2022-10-20 17:04:50 -0700

    Adding user to ubuntu docker group

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 git config --global core.editor "vim"
+git config --global user.name "Ben viP"
+git config --global user.email "bengg2040@gmail.com"
 
 sudo apt-get update
 
@@ -10,7 +12,7 @@ sudo apt install certbot python3-certbot-nginx npm
 # python-base
 sudo apt install libffi-dev cmake libjpeg-dev zlib1g-dev
 
-
+sudo usermod -aG docker $USER
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
