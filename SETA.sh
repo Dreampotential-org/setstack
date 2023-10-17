d9a22f2abc5d268354ec22982f45277ae0076899
arosen <arosen@node1.long-term2.rethinkdb-pg0.clemson.cloudlab.us>
2023-10-17 19:21:36 -0400

    update code base

--- SETA.sh
+++ SETA.sh
@@ -8,54 +8,94 @@ ssh-add ben
 sudo docker network create chiroposture_network
 sudo docker network create meylorCI
 
-# desktop
+
 cd ~
-git clone git@gitlab.com:devs176/docker-ubuntu.git
-cd docker-ubuntu
+git clone git@github.com:Dreampotential-org/meylordrive
+cd ~/meylordrive
+sudo ./scripts/start.sh
+sudo ./scripts/local_db.sh
+cd ~
+
+# postvideo
+git clone git@github.com:Dreampotential-org/postvideo
+cd postvideo
 sudo bash start.sh
+cd ~
 
 
 # agentstat
-git clone git@gitlab.com:a4496/agentstat.git
+git clone git@github.com:Dreampotential-org/agentstat
 cd agentstat
 sudo bash scripts/start.sh
 cd ~
 
+# agentstat-svelte
+git clone git@github.com:Dreampotential-org/agentstat-svelte
+cd agentstat-svelte
+sudo bash scripts/start.sh
+cd ~
+
+
+
+
 # useiam services
-git clone git@gitlab.com:devs176/useiam.git
-cd ~/useiam
-sudo ./scripts/start-prod.sh
-cd ~
+# git clone git@gitlab.com:devs176/useiam.git
+# cd ~/useiam
+# sudo ./scripts/start-prod.sh
+# cd ~
 
-git clone git@gitlab.com:devs176/useiam-server.git
-cd ~/useiam-server
-sudo ./scripts/start.sh
-cd ~
+# git clone git@gitlab.com:devs176/useiam-server.git
+# cd ~/useiam-server
+# sudo ./scripts/start.sh
+# cd ~
 
-# django zillow services
-git clone git@gitlab.com:a4496/python-base.git
+git clone git@github.com:Dreampotential-org/python-base
 cd ~/python-base
 sudo ./scripts/local_db.sh
 cd ~
 
 git clone git@gitlab.com:a4496/django-zillow.git
 cd ~/django-zillow
+sudo apt-get install nginx
 sudo ./scripts/start.sh
 sudo ./scripts/start-pg-bouncer.sh
 sudo ./scripts/start-db.sh
+sudo cp -r etc-letsencrypt/ /etc/letsencrypt/
+sudo cp -r conf.d/ /etc/nginx/conf.d/jj@jj-HP-Laptop-15-dy2xxx
 # start-es.sh
 cd ~
 
 
-git clone git@gitlab.com:devs176/teacher-ui.git
+git clone git@github.com:Dreampotential-org/teacher-ui
 cd ~/teacher-ui/
 sudo ./scripts/start.sh
 cd ~
 
-git clone git@gitlab.com:a4496/meylorci.git
-cd ~/meylorci
-sudo ./scripts/start.sh
-sudo ./scripts/local_db.sh
+git clone git@github.com:Dreampotential-org/teacher-ui-react
+cd ~/teacher-ui/
+sudo bash start.sh
 cd ~
+
+
+git clone git@github.com:Dreampotential-org/dreampotential-site
+cd ~/teacher-ui/
+sudo bash start.sh
+cd ~
+
+git clone git@github.com:Dreampotential-org/socket-link
+cd ~/socket-link/
+sudo docker-compose -f docker-compose.yml up -d
+cd ~
+
+
+git clone git@github.com:Dreampotential-org/docker-jitsi
+cd ~/docker-jitsi
+sudo bash start.sh
+cd ~
+
+
+
+
+
 
 
